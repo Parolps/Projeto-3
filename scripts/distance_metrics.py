fd6dc9417706fd75327e2c7678cc49a3715be8f7
Parolps <samuelmassas604@gmail.com>
2023-12-06 14:36:32 +0000

    first commit

---
+++ scripts/distance_metrics.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.metrics.pairwise import pairwise_distances
+
+pd.options.display.max_columns = 999
+
+# Read data
+df = pd.read_csv("data/drd2_data.csv")
+
+# Get X and y
+# X = df.iloc[:, :-1]
+X = df.iloc[:, :-1][[i for i in df.columns if "D" not in i]]
+y = df[["target"]]
+
+# Pairwise distances
+dist = pairwise_distances(X, metric="euclidean")
+
+# PCA
+pca = PCA(n_components=3)
+X_pca = pca.fit_transform(X)
+
+# Plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+
+scatter = ax.scatter(
+    X_pca[:, 0],
+    X_pca[:, 1],
+    X_pca[:, 2],
+    c=y.values.ravel(),
+    cmap="viridis",
+    edgecolor="k",
+    s=20,
+)
+
+ax.set_xlabel("PC1")
+ax.set_ylabel("PC2")
+ax.set_zlabel("PC3")
+ax.set_title(f"Explained Variance: {pca.explained_variance_ratio_.mean():.3f}")
+fig.colorbar(scatter)
+plt.show()
